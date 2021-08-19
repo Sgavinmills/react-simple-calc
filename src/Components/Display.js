3c6086f32701165f5b44c51dec6e87deaf8f14e5
Christopher Pearson <cjpearson@hotmail.com>
2021-08-19 14:37:18 +0100

    initial setup and layout

---
+++ src/Components/Display.js
@@ -0,0 +1,10 @@
+const Display = () => {
+    return (
+        <div className="Display">
+            <p>1738+17619-</p>
+            <h2>6282</h2>
+        </div>
+    );
+};
+
+export default Display;
