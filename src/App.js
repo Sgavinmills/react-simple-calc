20dceee3e6dc9a40c6a311ede241fcb1eee3fd37
Christopher Pearson <cjpearson@hotmail.com>
2021-08-19 17:11:06 +0100

    update states and props in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import './App.css';
-import Buttons from './Components/Buttons';
-import Display from './Components/Display';
-import Header from './Components/Header';
+import { useState } from "react";
+import "./App.css";
+import Buttons from "./Components/Buttons";
+import Display from "./Components/Display";
+import Header from "./Components/Header";
 
 function App() {
+  const [liveDisplay, setLiveDisplay] = useState('');
+  const [formulaDisplay, setFormulaDisplay] = useState('');
+
   return (
     <div className="App">
       <Header />
-      <Display />
-      <Buttons />
+      <Display liveDisplay={liveDisplay} formulaDisplay={formulaDisplay} />
+      <Buttons liveDisplay={liveDisplay} formulaDisplay={formulaDisplay} setLiveDisplay={setLiveDisplay} setFormulaDisplay={setFormulaDisplay} />
     </div>
   );
 }
